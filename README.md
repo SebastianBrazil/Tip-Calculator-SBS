44b5da73a2ed62797b55d8d179745b9092e62b5c
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-04-02 02:17:48 -0700

    logic

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Tip Calculator
 Desc
 
 4/1/24 pt1: Made the basic layout for the page and its elements. Styling and sizing are still needed.
+4/1/24 pt2: Decided to temporarily suspend styling to work on logic. It is, in all effect, done, but some formatting with zeros is still needed.
 
 Peer Review:
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,66 @@
+'use client'
+
 import Image from "next/image";
 import splitter from "@/app/assets/images/logo.svg"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [bill, setBill] = useState<string>("");
+  const [tipP, setTipP] = useState<string>("");
+  const [custTipP, setCusTipP] = useState<string>("");
+  const [peeps, setPeeps] = useState<string>("");
+  const [isPeepsZero, setIsPeepsZero] = useState<boolean>();
+
+  const [totalPerPerson, setTotalPerPerson] = useState<string>();
+  const [tipOnlyAmount, setTipOnlyAmount] = useState<string>();
+
+  const calculate = () => {
+    if (peeps === "0") {
+      setIsPeepsZero(true);
+      setTotalPerPerson(undefined);
+      setTipOnlyAmount(undefined);
+    } else {
+      let tipPHolder: string;
+      if (custTipP !== "") {
+        tipPHolder = custTipP;
+      } else {
+        tipPHolder = tipP;
+      }
+
+      setIsPeepsZero(false);
+      if (bill !== "" && bill !== "0" && tipPHolder !== "" && peeps !== "") {
+        if (tipPHolder === "0") {
+          setTotalPerPerson(String(Number(bill) / Number(peeps)));
+          setTipOnlyAmount(undefined);
+        } else {
+          let tipAmount = Number(bill) * (Number(tipPHolder) / 100)
+          let totAmount = Number(bill) + tipAmount
+          let totPerPerson = totAmount / Number(peeps)
+          let tipPerPerson = tipAmount / Number(peeps)
+          setTotalPerPerson(String(totPerPerson));
+          setTipOnlyAmount(String(tipPerPerson));
+        }
+      } else {
+        setTotalPerPerson(undefined);
+        setTipOnlyAmount(undefined);
+      }
+    }
+  };
+
+  const resetValues = () => {
+    setBill("");
+    setTipP("");
+    setCusTipP("");
+    setPeeps("");
+    setIsPeepsZero(false);
+    setTotalPerPerson(undefined);
+    setTipOnlyAmount(undefined);
+  }
+
+  useEffect(() => {
+    calculate();
+  }, [bill, tipP, peeps, custTipP])
+
   return (
     <div className="bg-[#c5e4e7]">
       <div className="flex justify-center pt-36 -mb-36">
@@ -11,24 +70,27 @@ export default function Home() {
       <div className="min-h-screen grid place-items-center">
         <div className="bg-white w-8/12 h-3/6 rounded-[30px] grid grid-cols-2">
           <div className="my-10 mr-4 ml-8">
-            <p>Bill</p>
-            <input type="text" />
+            <p className="">Bill</p>
+            <input value={bill} onChange={(e) => setBill(e.target.value)} className="w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
 
             <p>Select Tip %</p>
             <div className="grid grid-cols-3">
-              <button>5%</button>
-              <button>10%</button>
-              <button>15%</button>
-              <button>25%</button>
-              <button>50%</button>
+              <button onClick={() => { setCusTipP(""); setTipP("5") }}>5%</button>
+              <button onClick={() => { setCusTipP(""); setTipP("10") }}>10%</button>
+              <button onClick={() => { setCusTipP(""); setTipP("15") }}>15%</button>
+              <button onClick={() => { setCusTipP(""); setTipP("25") }}>25%</button>
+              <button onClick={() => { setCusTipP(""); setTipP("50") }}>50%</button>
+              <input value={custTipP} onChange={(e) => { setTipP(""); setCusTipP(e.target.value) }} className="w-12 rounded-md h-12 bg-[#f4fafa]" type="number" placeholder='Custom' />
               <button>Custom</button>
             </div>
 
             <div className="flex justify-between">
               <p>Number Of People</p>
-              <p>Can't be zero</p>
+              {
+                isPeepsZero && <p>Can't be zero</p>
+              }
             </div>
-            <input type="text" />
+            <input value={peeps} onChange={(e) => setPeeps(e.target.value)} className="w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
           </div>
 
           <div className="bg-[#00494d] my-10 ml-4 mr-8">
@@ -38,18 +100,25 @@ export default function Home() {
                 <p>/ person</p>
               </div>
               <div>
-                <p>$0.00</p>
-              </div>
+                {
+                  tipOnlyAmount ? <p className="text-[#26c0ab]">${tipOnlyAmount}</p> : <p className="text-[#26c0ab]">$0.00</p>
+                }              </div>
             </div>
 
             <div className="flex justify-between">
               <div>
-                <p>Tip Amount</p>
-                <p>/ person</p>
+                <p className="">Total</p>
+                <p className="">/ person</p>
               </div>
               <div>
-                <p>$0.00</p>
+                {
+                  totalPerPerson ? <p className="text-[#26c0ab]">${totalPerPerson}</p> : <p className="text-[#26c0ab]">$0.00</p>
+                }
               </div>
+            </div>
+
+            <div className="flex justify-center">
+              <button onClick={() => resetValues()}>Reset</button>
             </div>
 
           </div>
