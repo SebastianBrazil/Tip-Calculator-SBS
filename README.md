fb0185c957e1feb9fcb7a2f744dc9d8f92f84c63
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-04-03 14:01:48 -0700

    hovers

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ Desc
 4/2/24 pt1: Added some more logic which formats zeros, but that is practically styling. As of now the logic is completely complete, with styling being the next goal.
 4/2/24 pt2: Added a lot of desktop styling as well as made some logic modifications. Responsiveness, hovers and active states, and some more desktop styling are still needed.
 4/2/24 pt3: All of the styling is done, the next thing to work on is the hover and focus states.
+4/3/24 pt1: Finished all of the hovers and focus states, with some more logic sprinkled in. Finally mobile can commence.
 
 Peer Review:
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,28 @@ export default function Home() {
   const [totalPerPerson, setTotalPerPerson] = useState<string>();
   const [tipOnlyAmount, setTipOnlyAmount] = useState<string>();
 
+  const [isButtonSelected1, setIsButtonSelected1] = useState<boolean>(false);
+  const [isButtonSelected2, setIsButtonSelected2] = useState<boolean>(false);
+  const [isButtonSelected3, setIsButtonSelected3] = useState<boolean>(false);
+  const [isButtonSelected4, setIsButtonSelected4] = useState<boolean>(false);
+  const [isButtonSelected5, setIsButtonSelected5] = useState<boolean>(false);
+
   const calculate = () => {
-    if (peeps === "0") {
+    let isAllPeepsZero: boolean = false;
+    if (peeps.length !== 0) {
+      let splitPeeps = peeps.split("");
+      let boolValues: boolean[] = [];
+
+      splitPeeps.forEach(number => {
+        boolValues.push(Number(number) === 0)
+      })
+
+      if (boolValues.indexOf(false) === -1) {
+        isAllPeepsZero = true;
+      }
+    }
+
+    if (isAllPeepsZero === true) {
       setIsPeepsZero(true);
       setTotalPerPerson(undefined);
       setTipOnlyAmount(undefined);
@@ -32,7 +52,17 @@ export default function Home() {
       setIsPeepsZero(false);
       if (bill !== "" && bill !== "0" && tipPHolder !== "" && peeps !== "") {
         if (tipPHolder === "0") {
-          setTotalPerPerson(String(Number(bill) / Number(peeps)));
+          let totalPre = String(Number(bill) / Number(peeps))
+
+          if (totalPre.indexOf(".") !== -1) {
+            let checkEndSolo = totalPre.split(".")
+            let midResultSolo = checkEndSolo[1].padEnd(2, "0")
+            totalPre = checkEndSolo[0] + "." + midResultSolo;
+          } else {
+            totalPre = totalPre + ".00";
+          }
+
+          setTotalPerPerson(totalPre);
           setTipOnlyAmount(undefined);
         } else {
           let tipAmount = Number(bill) * (Number(tipPHolder) / 100)
@@ -77,6 +107,54 @@ export default function Home() {
     setIsPeepsZero(false);
     setTotalPerPerson(undefined);
     setTipOnlyAmount(undefined);
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(false);
+  }
+
+  const activateBtn1 = () => {
+    setIsButtonSelected1(true);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(false);
+  }
+  const activateBtn2 = () => {
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(true);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(false);
+  }
+  const activateBtn3 = () => {
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(true);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(false);
+  }
+  const activateBtn4 = () => {
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(true);
+    setIsButtonSelected5(false);
+  }
+  const activateBtn5 = () => {
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(true);
+  }
+  const deactivateBtns = () => {
+    setIsButtonSelected1(false);
+    setIsButtonSelected2(false);
+    setIsButtonSelected3(false);
+    setIsButtonSelected4(false);
+    setIsButtonSelected5(false);
   }
 
   useEffect(() => {
@@ -95,7 +173,7 @@ export default function Home() {
             <div>
               <p className="text-[#00494d] text-sm pb-1">Bill</p>
               <div>
-                <input value={bill} onChange={(e) => setBill(e.target.value)} className="pr-4 spaceMonoB text-2xl text-[#00494d] text-right w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
+                <input value={bill} onChange={(e) => setBill(e.target.value)} className="input2 pr-4 spaceMonoB text-2xl text-[#00494d] text-right w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
                 <img className="-mt-7 mb-[11px] ml-4" src={dollan.src} alt="" />
               </div>
             </div>
@@ -103,26 +181,38 @@ export default function Home() {
             <div>
               <p className="text-[#00494d] pb-4">Select Tip %</p>
               <div className="grid grid-cols-3 gap-4">
-                <button className="rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { setCusTipP(""); setTipP("5") }}>5%</button>
-                <button className="rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { setCusTipP(""); setTipP("10") }}>10%</button>
-                <button className="rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { setCusTipP(""); setTipP("15") }}>15%</button>
-                <button className="rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { setCusTipP(""); setTipP("25") }}>25%</button>
-                <button className="rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { setCusTipP(""); setTipP("50") }}>50%</button>
                 {
-                  custTipP ? <input value={custTipP} onChange={(e) => { setTipP(""); setCusTipP(e.target.value) }} className="spaceMonoB text-[#00494d] text-2xl text-right w-full rounded-md bg-[#f4fafa] py-2 pr-4" type="number" placeholder='Custom' /> : <input value={custTipP} onChange={(e) => { setTipP(""); setCusTipP(e.target.value) }} className="spaceMonoB text-[#00494d] text-2xl text-center w-full rounded-md bg-[#f4fafa]" type="number" placeholder='Custom' />
+                  isButtonSelected1 ? <button className="hober rounded-md bg-[#26c0ab] py-2 text-[#00494d] text-2xl" onClick={() => { activateBtn1(); setCusTipP(""); setTipP("5") }}>5%</button> : <button className="hober rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { activateBtn1(); setCusTipP(""); setTipP("5") }}>5%</button>
                 }
-                </div>
+                {
+                  isButtonSelected2 ? <button className="hober rounded-md bg-[#26c0ab] py-2 text-[#00494d] text-2xl" onClick={() => { activateBtn2(); setCusTipP(""); setTipP("10") }}>10%</button> : <button className="hober rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { activateBtn2(); setCusTipP(""); setTipP("10") }}>10%</button>
+                }
+                {
+                  isButtonSelected3 ? <button className="hober rounded-md bg-[#26c0ab] py-2 text-[#00494d] text-2xl" onClick={() => { activateBtn3(); setCusTipP(""); setTipP("15") }}>15%</button> : <button className="hober rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { activateBtn3(); setCusTipP(""); setTipP("15") }}>15%</button>
+                }
+                {
+                  isButtonSelected4 ? <button className="hober rounded-md bg-[#26c0ab] py-2 text-[#00494d] text-2xl" onClick={() => { activateBtn4(); setCusTipP(""); setTipP("25") }}>25%</button> : <button className="hober rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { activateBtn4(); setCusTipP(""); setTipP("25") }}>25%</button>
+                }
+                {
+                  isButtonSelected5 ? <button className="hober rounded-md bg-[#26c0ab] py-2 text-[#00494d] text-2xl" onClick={() => { activateBtn5(); setCusTipP(""); setTipP("50") }}>50%</button> : <button className="hober rounded-md bg-[#00494d] py-2 text-[#f4fafa] text-2xl" onClick={() => { activateBtn5(); setCusTipP(""); setTipP("50") }}>50%</button>
+                }
+                {
+                  custTipP ? <input value={custTipP} onChange={(e) => { deactivateBtns(); setTipP(""); setCusTipP(e.target.value) }} className="input1 spaceMonoB text-[#00494d] text-2xl text-right w-full rounded-md bg-[#f4fafa] py-2 pr-4" type="number" placeholder='Custom' /> : <input value={custTipP} onChange={(e) => { deactivateBtns(); setTipP(""); setCusTipP(e.target.value) }} className="input1 spaceMonoB text-[#00494d] text-2xl text-center w-full rounded-md bg-[#f4fafa]" type="number" placeholder='Custom' />
+                }
+              </div>
             </div>
 
             <div>
               <div className="flex justify-between">
                 <p className="text-[#00494d] pb-1 text-sm">Number Of People</p>
                 {
-                  isPeepsZero && <p>Can't be zero</p>
+                  isPeepsZero && <p className="text-sm pb-1 text-[#dc755e]">Can't be zero</p>
                 }
               </div>
               <div className="">
-                <input value={peeps} onChange={(e) => setPeeps(e.target.value)} className="pr-4 spaceMonoB text-2xl text-[#00494d] text-right w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
+                {
+                  isPeepsZero ? <input value={peeps} onChange={(e) => setPeeps(e.target.value)} className="pr-4 spaceMonoB text-2xl text-[#00494d] text-right w-full rounded-md h-10 input2Zero bg-[#f4fafa]" type="number" placeholder='0' /> : <input value={peeps} onChange={(e) => setPeeps(e.target.value)} className="input2 pr-4 spaceMonoB text-2xl text-[#00494d] text-right w-full rounded-md h-10 bg-[#f4fafa]" type="number" placeholder='0' />
+                }
                 <img className="-mt-7 mb-[11px] ml-4" src={dollanDark.src} alt="" />
               </div>
             </div>
@@ -158,7 +248,9 @@ export default function Home() {
               </div>
 
               <div className="flex justify-center">
-                <button className="bg-[#26c0ab] h-12 rounded-md text-xl pt-[2px] mb-10 text-[#00494d] w-full spaceMonoB" onClick={() => resetValues()}>RESET</button>
+                {
+                  bill !== "" || tipP !== "" || peeps !== "" || custTipP !== "" ? <button className="hober bg-[#26c0ab] h-12 rounded-md text-xl pt-[2px] mb-10 text-[#00494d] w-full spaceMonoB" onClick={() => resetValues()}>RESET</button> : <button disabled className="bg-[#29686d] h-12 rounded-md text-xl pt-[2px] mb-10 text-[#245d62] w-full spaceMonoB" onClick={() => resetValues()}>RESET</button>
+                }
               </div>
             </div>
           </div>
